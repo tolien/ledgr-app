f0df44ce4c55322277d6b6ac3d15e095ff83ae6b
tolien <tolien@users.noreply.github.com>
2013-02-27 14:12:22 +0000

    Unit test to check some item uniqueness

--- test/unit/item_test.rb
+++ test/unit/item_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class ItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "item must be unique within all its categories" do
+      water = items(:water)
+      new_water = Item.create(name: water.name, category_ids: water.category_ids)
+      
+      assert new_water.invalid?
+      assert new_water.errors[:name].include?('must be unique in the category')
+  end
 end
