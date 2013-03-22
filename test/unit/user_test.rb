252c416204a2adb5b01a93e0deb03a340ddfb2be
Stuart Swindells <github@lumo.me.uk>
2013-03-22 10:29:30 +0000

    Add username field to users, make it unique, tests and fixtures for the above

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "user attributes need to be populated" do
+    user = User.new
+    assert user.invalid?
+    
+    assert user.errors[:username].any?
+    assert user.errors[:email].any?
+    assert user.errors[:password].any?
+    assert user.errors[:password_confirmation].any?
+  end
+  
+  test "user must have a matching password confirmation" do
+    user = User.new
+    assert user.invalid?
+    
+    user.password = 'password'
+    
+    assert user.errors[:password_confirmation].any?
+    user.password_confirmation = 'password12'
+    
+    assert user.errors[:password_confirmation].any?
+    user.password_confirmation = 'password'
+  end
 end
