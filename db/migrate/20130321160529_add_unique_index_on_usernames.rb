20a6019b174678c01e8a3238e15e4c5da4306a5f
tolien <tolien@users.noreply.github.com>
2013-03-22 10:29:30 +0000

    Add username field to users, make it unique, tests and fixtures for the above

---
+++ db/migrate/20130321160529_add_unique_index_on_usernames.rb
@@ -0,0 +1,9 @@
+class AddUniqueIndexOnUsernames < ActiveRecord::Migration
+  def up
+    add_index :users, :username, :unique => true
+  end
+
+  def down
+    remove_index :users, :username
+  end
+end
