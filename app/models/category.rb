4f0e983f67f91ed504729f12158409be53e96f9a
tolien <tolien@users.noreply.github.com>
2013-04-03 14:50:20 +0100

    Make user_id accessible on category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ActiveRecord::Base
-  attr_accessible :name
+  attr_accessible :name, :user_id
   
   has_and_belongs_to_many :items, join_table: 'item_categories'
   belongs_to :users
