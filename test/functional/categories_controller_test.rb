2374f2dd5b0d81a45d01fe62c07b9993df2526f7
tolien <tolien@users.noreply.github.com>
2013-04-03 14:07:05 +0100

    Update functional tests for nested resources

--- test/functional/categories_controller_test.rb
+++ test/functional/categories_controller_test.rb
@@ -3,47 +3,48 @@ require 'test_helper'
 class CategoriesControllerTest < ActionController::TestCase
   setup do
     @category = categories(:drinks)
+    @user = users(:one)
   end
 
   test "should get index" do
-    get :index
+    get :index, user_id: @user.id
     assert_response :success
     assert_not_nil assigns(:categories)
   end
 
   test "should get new" do
-    get :new
+    get :new, user_id: @user.id
     assert_response :success
   end
 
   test "should create category" do
     assert_difference('Category.count') do
-      post :create, category: { name: @category.name }
+      post :create, category: { name: @category.name, user_id: @user.id }, user_id: @user.id
     end
 
-    assert_redirected_to category_path(assigns(:category))
+    assert_redirected_to user_category_path(@user.id, assigns(:category))
   end
 
   test "should show category" do
-    get :show, id: @category
+    get :show, id: @category, user_id: @user.id
     assert_response :success
   end
 
   test "should get edit" do
-    get :edit, id: @category
+    get :edit, id: @category, user_id: @user.id
     assert_response :success
   end
 
   test "should update category" do
-    put :update, id: @category, category: { name: @category.name }
-    assert_redirected_to category_path(assigns(:category))
+    put :update, id: @category, category: { name: @category.name, user_id: @user.id }, user_id: @user.id
+    assert_redirected_to user_category_path(@user.id, assigns(:category))
   end
 
   test "should destroy category" do
     assert_difference('Category.count', -1) do
-      delete :destroy, id: @category
+      delete :destroy, id: @category, user_id: @user.id
     end
 
-    assert_redirected_to categories_path
+    assert_redirected_to user_categories_path
   end
 end
--- test/functional/entries_controller_test.rb
+++ test/functional/entries_controller_test.rb
@@ -2,48 +2,49 @@ require 'test_helper'
 
 class EntriesControllerTest < ActionController::TestCase
   setup do
-    @entry = entries(:one)
+    @entry = entries(:entryone)
+    @user = users(:one)
   end
 
   test "should get index" do
-    get :index
+    get :index, user_id: @user.id
     assert_response :success
     assert_not_nil assigns(:entries)
   end
 
   test "should get new" do
-    get :new
+    get :new, user_id: @user.id
     assert_response :success
   end
 
   test "should create entry" do
     assert_difference('Entry.count') do
-      post :create, entry: { datetime: @entry.datetime, quantity: @entry.quantity }
+      post :create, entry: { datetime: @entry.datetime, quantity: @entry.quantity }, user_id: @user.id
     end
 
-    assert_redirected_to entry_path(assigns(:entry))
+    assert_redirected_to user_entry_path(@user.id, assigns(:entry))
   end
 
   test "should show entry" do
-    get :show, id: @entry
+    get :show, id: @entry, user_id: @user.id
     assert_response :success
   end
 
   test "should get edit" do
-    get :edit, id: @entry
+    get :edit, id: @entry, user_id: @user.id
     assert_response :success
   end
 
   test "should update entry" do
-    put :update, id: @entry, entry: { datetime: @entry.datetime, quantity: @entry.quantity }
-    assert_redirected_to entry_path(assigns(:entry))
+    put :update, id: @entry, entry: { datetime: @entry.datetime, quantity: @entry.quantity }, user_id: @user.id
+    assert_redirected_to user_entry_path(@user.id, assigns(:entry))
   end
 
   test "should destroy entry" do
     assert_difference('Entry.count', -1) do
-      delete :destroy, id: @entry
+      delete :destroy, id: @entry, user_id: @user.id
     end
 
-    assert_redirected_to entries_path
+    assert_redirected_to user_entries_path(user_id: @user.id)
   end
 end
