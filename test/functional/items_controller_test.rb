7c3d12834be9d99abbe56600d355d4259cc156e2
Stuart Swindells <github@lumo.me.uk>
2013-04-03 14:07:05 +0100

    Update functional tests for nested resources

--- test/functional/items_controller_test.rb
+++ test/functional/items_controller_test.rb
@@ -3,47 +3,48 @@ require 'test_helper'
 class ItemsControllerTest < ActionController::TestCase
   setup do
     @item = items(:water)
+    @user = users(:one)
   end
 
   test "should get index" do
-    get :index
+    get :index, user_id: @user.id
     assert_response :success
     assert_not_nil assigns(:items)
   end
 
   test "should get new" do
-    get :new
+    get :new, user_id: @user.id
     assert_response :success
   end
 
   test "should create item" do
     assert_difference('Item.count') do
-      post :create, item: { name: @item.name }
+      post :create, user_id: @item.user.id, item: { name: @item.name }
     end
 
-    assert_redirected_to item_path(assigns(:item))
+    assert_redirected_to user_item_path(@item.user.id, assigns(:item))
   end
 
   test "should show item" do
-    get :show, id: @item
+    get :show, id: @item, user_id: @user.id
     assert_response :success
   end
 
   test "should get edit" do
-    get :edit, id: @item
+    get :edit, id: @item, user_id: @user.id
     assert_response :success
   end
 
   test "should update item" do
-    put :update, id: @item, item: { name: @item.name }
-    assert_redirected_to item_path(assigns(:item))
+    put :update, id: @item, item: { name: @item.name }, user_id: @user.id
+    assert_redirected_to user_item_path(@user.id, assigns(:item))
   end
 
   test "should destroy item" do
     assert_difference('Item.count', -1) do
-      delete :destroy, id: @item
+      delete :destroy, id: @item, user_id: @user.id
     end
 
-    assert_redirected_to items_path
+    assert_redirected_to user_items_path(@user.id)
   end
 end
