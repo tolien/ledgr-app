7ce876326d5b27b0fa83c30768269ef710870584
Stuart Swindells <github@lumo.me.uk>
2013-04-07 22:55:38 +0100

    Test validations on Entry

--- test/unit/entry_test.rb
+++ test/unit/entry_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class EntryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "An entry must be valid" do
+    entry = Entry.create()
+    assert entry.invalid?
+    assert entry.errors[:quantity].include?('is not a number')
+    assert entry.errors[:item].include?('can\'t be blank')
+    assert entry.errors[:datetime].include?('is not a valid datetime')
+    assert entry.errors[:user].include?('can\'t be blank')
+    
+    
+    entry.quantity = 0
+    entry.item_id = items(:water).id
+    entry.datetime = Time.now
+    entry.user_id = users(:one).id
+    assert entry.valid?
+  end
 end
