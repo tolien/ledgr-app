456c7238fb079f4437c3666d0044c92030eb3194
tolien <tolien@users.noreply.github.com>
2013-04-12 11:04:59 +0100

    Add slug to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ActiveRecord::Base
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :token_authenticatable
+         
+  extend FriendlyId
+  friendly_id :username, use: :slugged
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :username
@@ -14,7 +17,7 @@ class User < ActiveRecord::Base
   has_many :entries
 
   validates_uniqueness_of :username  
-  validates_presence_of :password_confirmation
+  validates_presence_of :password_confirmation, on: :create
   validates_presence_of :username
 
 end
