02f190adc2b9480daf38f8f895a20b7f900afb74
tolien <tolien@users.noreply.github.com>
2013-04-15 14:21:22 +0100

    A user can only have one category with a given name

--- test/unit/category_test.rb
+++ test/unit/category_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user_one = users(:one)
+    @user_two = users(:two)
+    @drinks = categories(:drinks)
+  end
+  
+  test "categories should be unique per-user" do
+    drinks = Category.create(name: @drinks.name, user_id: @drinks.user_id)
+    assert drinks.invalid?
+    assert drinks.errors[:name].include?("has already been taken")
+    
+    drinks.user_id = @user_two.id
+    assert drinks.valid?
+  end
 end
