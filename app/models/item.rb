171ae663ec502c000b973a05c123a07d05336ef9
tolien <tolien@users.noreply.github.com>
2013-05-03 11:35:54 +0100

    Add method to find an existing item or create a new one if one does not exist

--- app/models/item.rb
+++ app/models/item.rb
@@ -19,5 +19,17 @@ class Item < ActiveRecord::Base
       errors[:categories] << "Item can only be a member of a category once"
     end
   end  
-
+  
+  def self.find_or_create_by_user_and_name(user, name)
+  logger.debug('user_id: ' + user.id.to_s)
+  logger.debug('item name: ' + name)
+  
+    item = Item.find_by_name_and_user_id(name, user.id)
+    
+    if item.nil?
+      logger.debug('creating new item')
+      item = Item.create(name: name, user_id: user.id)
+    end
+    item
+  end
 end
