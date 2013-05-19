2915f302277eeae239db3b623a4692c0327e2ef0
Stuart Swindells <github@lumo.me.uk>
2013-05-19 23:04:32 +0100

    Import now loads entries

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,8 +7,9 @@ module ApplicationHelper
     user = User.find(user_id)
     Rails.logger.info("Found user with username #{user.username}")
     count = 0
-    old_category_count = Category.where("user_id = #{user_id}").count
-    old_item_count = Item.where("user_id = #{user_id}").count
+    old_category_count = Category.where("user_id = ?", user_id).count
+    old_item_count = Item.where("user_id = ?", user_id).count
+    old_entry_count = Entry.where("user_id = ?", user_id).count
     
     category_name_list = []
     item_name_list = []
@@ -42,19 +43,18 @@ module ApplicationHelper
             item_categories << category_name
           end
           item_category_map[item_name] = item_categories
-          Rails.logger.info("Item #{item_name} has #{item_categories.count} categories")
         end
       end
       
       # jam entries into a list-of-arrays
       # TODO: better way of doing this kthx      
-      entry_name_list << [row['name'], row['amount'], row['datetime']]        
+      entry_name_list << [row['name'], row['amount'].to_f, row['date'].to_datetime]        
       
       count = count + 1
     end
       
-    Rails.logger.info("Read #{category_name_list.count} unique category names")
-    Rails.logger.info("Read #{item_name_list.count} unique item names")
+    Rails.logger.debug("Read #{category_name_list.count} unique category names")
+    Rails.logger.debug("Read #{item_name_list.count} unique item names")
     
     # turn the list of category names into a list of Category objects
     category_list = []
@@ -74,40 +74,58 @@ module ApplicationHelper
       Item.import item_list
       
       # then associate items with categories
-      Item.where("user_id = #{user.id}").each do |item|
-        Rails.logger.info("Loading categories for #{item.name}")
+      Item.where("user_id = ?", user.id).each do |item|
+        Rails.logger.debug("Loading categories for #{item.name}")
         item_name = item.name
         item_categories = item_category_map[item_name]
         if item_categories
-          Rails.logger.info("There are #{item_categories.count} categories for this item.")
+          Rails.logger.debug("There are #{item_categories.count} categories for this item.")
           item_categories.each do |category_name|
-            category = Category.where("user_id = #{user.id} AND name = '#{category_name}'")
-            Rails.logger.info("Adding category #{category.name}")
+            category = Category.where("user_id = ? AND name = ?", user.id, category_name)
+            Rails.logger.debug("Adding category #{category.name}")
             item.add_category(category)
           end
         end
       end
     end
     
+    
+    Rails.logger.info("Loading entries")
     # now load entries
     entry_list = []
+    item_id_map = {}
     entry_name_list.each do |entry_name|
-      item_id = Item.find_by_name_and_user_id(entry_name[0], user.id).id
-      entry_list << Entry.new(item_id: item_id, quantity: entry_name[1], datetime: entry_name[2])
+      item_name = entry_name[0]
+      item_id = item_id_map[item_name]
+      if item_id.nil?
+        Rails.logger.debug("Haven't seen item with name #{item_name} before.")
+        item_id = Item.find_by_name_and_user_id(item_name, user.id).id
+        item_id_map[item_name] = item_id
+      end
+      #Rails.logger.debug("Creating entry with values user_id: #{user_id}, item_id: #{item_id}, quantity: #{entry_name[1]}, datetime: #{entry_name[2]}")
+       entry_list << Entry.new(user_id: user_id, item_id: item_id, quantity: entry_name[1], datetime: entry_name[2])
     end
     
+    Rails.logger.debug("Loading #{entry_list.count} entries.")
     Entry.transaction do
-      Entry.import entry_list
+      entry_list.each_slice(500) do |slice|
+        result = Entry.import slice, validate: :false
+        if !result.failed_instances.empty?
+          Rails.logger.info("Failed to load #{result.failed_instances.count} records.")
+        end
+      end
     end
-    
-    category_count = Category.where("user_id = #{user_id}").count
-    item_count = Item.where("user_id = #{user_id}").count
+        
+    category_count = Category.where("user_id = ?", user_id).count
+    item_count = Item.where("user_id = ?", user_id).count
     end_time = Time.now
+    entry_count = Entry.where("user_id = ?", user_id).count
     time_taken = end_time - start_time
     
     Rails.logger.info("Imported #{count} rows.")
     Rails.logger.info("Created #{category_count - old_category_count} categories")
     Rails.logger.info("Created #{item_count - old_item_count} items")
+    Rails.logger.info("Created #{entry_count - old_entry_count} entries")
     Rails.logger.info("#{time_taken} seconds (#{(count / time_taken).round(0)} items/second).")
     
   end
