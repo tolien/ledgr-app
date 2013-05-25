582ca7d8a4c76645f59e3d754ce461fbaa5964c8
Stuart Swindells <github@lumo.me.uk>
2013-05-25 22:23:03 +0100

    More validation for the category model

--- test/unit/category_test.rb
+++ test/unit/category_test.rb
@@ -7,6 +7,18 @@ class CategoryTest < ActiveSupport::TestCase
     @drinks = categories(:drinks)
   end
   
+  test "category validation" do
+    category = Category.create
+
+    assert category.invalid?    
+    assert category.errors[:name].include? "can't be blank"
+    assert category.errors[:user].include? "can't be blank"
+    
+    category.user_id = @user_two.id
+    category.name = "wibble"
+    assert category.valid?
+  end
+  
   test "categories should be unique per-user" do
     drinks = Category.create(name: @drinks.name, user_id: @drinks.user_id)
     assert drinks.invalid?
