d75d73dd2dbb393618f1be4f3de601bcddd90a12
tolien <tolien@users.noreply.github.com>
2013-05-30 22:10:33 +0100

    Default sort order for Category and Item (ascending by name)

--- app/models/category.rb
+++ app/models/category.rb
@@ -9,6 +9,8 @@ class Category < ActiveRecord::Base
   validates_presence_of :name, :user
   validates :name, uniqueness: { scope: :user_id }
   
+  default_scope order('name ASC')
+  
   def self.find_or_create_by_user_and_name(user, name)
   logger.debug('user_id: ' + user.id.to_s)
   logger.debug('category name: ' + name)
