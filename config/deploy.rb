d919f88b385f24df3eae886e76c065abe7fe907b
tolien <tolien@users.noreply.github.com>
2013-06-05 10:45:40 +0100

    Add some handy deployment tasks

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,10 +26,21 @@ set :use_sudo, false
 # these http://github.com/rails/irs_process_scripts
 
 # If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+  task :seed do
+    run "cd #{current_path}; #{rake} db:seed RAILS_ENV=#{rails_env}"
+  end
+end
+
+namespace :bundle do
+  desc "run bundle install and ensure all gem requirements are met"
+  task :install do
+    run "cd #{current_path} && #{try_sudo} bundle install  --without=test"
+  end
+end
+#before "deploy:restart", "bundle:install"
