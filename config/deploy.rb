834eb8cc51cb81e5013cc6d41d19b43e302bfa27
tolien <tolien@users.noreply.github.com>
2013-06-05 10:42:03 +0100

    Deployment config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,23 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+require 'secret_token_replacer/recipes'
+
+set :application, "data-tracker"
+set :repository,  "git@github.com:tolien/data-tracker.git"
+set :domain, "ra.tolien.co.uk"
+
+set :deploy_to, "/var/sites/#{application}" 
+
+load 'deploy/assets'
 
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+role :web, domain                          # Your HTTP server, Apache/etc
+role :app, domain                          # This may be the same as your `Web` server
+role :db,  domain, :primary => true # This is where Rails migrations will run
+
+# you might need to set this if you aren't seeing password prompts
+default_run_options[:pty] = true
+set :use_sudo, false
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
