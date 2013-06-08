721e399449bdd5db00b47ad98b3bb475de86d7ab
Stuart Swindells <github@lumo.me.uk>
2013-06-08 21:59:56 +0100

    Fix foreign key creation

--- db/migrate/20130301101545_add_foreign_keys.rb
+++ db/migrate/20130301101545_add_foreign_keys.rb
@@ -2,7 +2,6 @@ class AddForeignKeys < ActiveRecord::Migration
   def change
     add_foreign_key "categories", "users", :name => "categories_user_id_fk", dependent: :delete
     add_foreign_key "entries", "items", :name => "entries_item_id_fk", dependent: :delete
-    add_foreign_key "entries", "users", :name => "entries_user_id_fk", dependent: :delete
     add_foreign_key "item_categories", "categories", :name => "item_categories_category_id_fk", dependent: :delete
     add_foreign_key "item_categories", "items", :name => "item_categories_item_id_fk", dependent: :delete
     add_foreign_key "items", "users", :name => "items_user_id_fk", dependent: :delete
