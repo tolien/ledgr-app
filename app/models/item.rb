a0ae68280015ca1bdcadd9f67c53b22a00181891
tolien <tolien@users.noreply.github.com>
2013-06-13 09:06:58 +0100

    Put default_scope inside a block to silence deprecation warning

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,7 +12,7 @@ class Item < ActiveRecord::Base
   validates_associated :item_categories
   validates_presence_of :user
 
-  default_scope order('name ASC')
+  default_scope { order('name ASC') }
 
   def add_category(category)
     if !categories.include? category
