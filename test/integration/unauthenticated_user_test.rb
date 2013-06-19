ce7ebfe7cfa285990b05d42f2a473b16426e8d88
Stuart Swindells <github@lumo.me.uk>
2013-06-19 15:25:29 +0100

    Add slugs to user test fixtures and flesh out a basic integration test for the entries index and items index pages

--- test/integration/unauthenticated_user_test.rb
+++ test/integration/unauthenticated_user_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class UnauthenticatedUserTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :all
+  
+  test "user browsing" do
+     get "/#{users(:one).username}/entries"
+     assert_response :success
+     assert_template "index"
+     assert_select 'table tr', users(:one).entries.count
+     
+     get "/#{users(:one).username}/items"
+     assert_response :success
+     assert_template "index"
+     assert_select 'table tr', users(:one).items.count
+  end
 end
