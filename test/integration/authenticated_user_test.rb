6ccf789753cf0740542c8fee2dd61a1bec6af3bb
tolien <tolien@users.noreply.github.com>
2013-07-18 13:40:42 +0100

    Integration test for the new-item-from-a-category

--- test/integration/authenticated_user_test.rb
+++ test/integration/authenticated_user_test.rb
@@ -3,8 +3,24 @@ require 'test_helper'
 class AuthenticatedUserTestTest < ActionDispatch::IntegrationTest
   fixtures :all
   
+  def sign_in(user, password)
+    post_via_redirect user_session_path, username: user.username, password: password
+  end
+    
   def setup
     @user_one = users(:one)
-    sign_in @user_one
+    @category = categories(:drinks)
+    @item = @category.items.first
+    sign_in @user_one, 'password'
+  end
+  
+  test "create items from the category show page" do
+    get "/#{@user_one.username}/categories/#{@category.id}"
+    assert_select 'a[data-method="delete"]'
+    
+    get "/#{@user_one.username}/items/new?category_id=#{@category.id}"
+    assert_select "input#item_category_id" do
+      assert_select '[value=?]', @category.id.to_s
+    end
   end
 end
