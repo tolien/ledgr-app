8ed8a21ad2a62ee00dba38ff38d8f5c1883d3b91
tolien <tolien@users.noreply.github.com>
2013-07-19 16:51:07 +0100

    Only want the Display-related FKs in this migration

--- db/migrate/20130719102332_add_display_fks.rb
+++ db/migrate/20130719102332_add_display_fks.rb
@@ -1,9 +1,7 @@
-class AddMoreFks < ActiveRecord::Migration
+class AddDisplayFks < ActiveRecord::Migration
   def change
     add_foreign_key "display_categories", "categories", :name => "display_categories_category_id_fk"
     add_foreign_key "display_categories", "displays", :name => "display_categories_display_id_fk"
     add_foreign_key "displays", "display_types", :name => "displays_display_type_id_fk"
-    add_foreign_key "item_categories", "categories", :name => "item_categories_category_id_fk"
-    add_foreign_key "item_categories", "items", :name => "item_categories_item_id_fk"
   end
 end
