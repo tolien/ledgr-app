22411dfe6b73df1566c6f433e4d11d9019889d39
tolien <tolien@users.noreply.github.com>
2013-07-28 01:03:27 +0100

    Changes to items controller to get tests passing

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -29,6 +29,10 @@ class ItemsController < ApplicationController
   # GET /items/new.json
   def new
     @user = User.find(params[:user_id])
+    unless current_user.id == @user.id
+      render status: :forbidden, text: "You may not create items for someone else"
+      return
+    end
     @item = @user.items.build
     @category_id = params[:category_id]
     unless @category_id.nil?
@@ -53,11 +57,17 @@ class ItemsController < ApplicationController
     @item = Item.new(name: params[:item][:name], user_id: User.find(params[:user_id]).id)
     @user = User.find(params[:user_id])
     @item.user = @user
+    
+    unless current_user.id == @user.id
+      render status: :forbidden, text: "You may not create items for someone else"
+      return
+    end
 
     respond_to do |format|
       if @item.save
-        if(params[:item][:category_id])
-          category = Category.where('id = ? and user_id = ?', params[:item][:category_id].to_i, @user.id).first
+        category_id = params[:item][:category_id]
+        if(!category_id.nil? and category_id.length > 0)
+          category = Category.where('id = ? and user_id = ?', category_id.to_i, @user.id).first
           Rails.logger.info("Category ID: #{category.id}")
           @item.add_category(category)
           
@@ -78,6 +88,11 @@ class ItemsController < ApplicationController
   def update
     @item = Item.find(params[:id])
     @user = @item.user
+    
+    unless current_user.id == @user.id
+      render status: :forbidden, text: "You may not update an item belonging to someone else"
+      return
+    end
 
     respond_to do |format|
       if @item.update_attributes(params[:item])
@@ -94,12 +109,11 @@ class ItemsController < ApplicationController
   # DELETE /items/1.json
   def destroy
     @item = Item.find(params[:id])
-    if current_user.id == @item.user.id
-      @item.destroy
-    else
+    unless current_user.id == @item.user.id
       render status: :forbidden, text: "You don't own this item!"
       return
     end
+    @item.destroy
 
     respond_to do |format|
       format.html { redirect_to user_items_url }
