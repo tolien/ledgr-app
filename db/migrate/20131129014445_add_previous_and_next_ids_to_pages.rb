3de165b83dad5c9bfdc0bebf939d4ce193586b28
tolien <tolien@users.noreply.github.com>
2013-11-29 14:49:53 +0000

    Remove migration to add previous/next page IDs to Pages table

--- db/migrate/20131129014445_add_previous_and_next_ids_to_pages.rb
+++
@@ -1,9 +0,0 @@
-class AddPreviousAndNextIdsToPages < ActiveRecord::Migration
-  def change
-    add_column :pages, :previous_id, :integer
-    add_column :pages, :next_id, :integer
-    
-    add_foreign_key :pages, :pages, column: "previous_id"
-    add_foreign_key :pages, :pages, column: "next_id"
-  end
-end
