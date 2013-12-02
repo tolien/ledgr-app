c03ef482e9f25f2076c1c1a9bc3b50715cd9609f
tolien <tolien@users.noreply.github.com>
2013-12-02 01:19:34 +0000

    Add unit tests for the Page model, asserting that a page must have an owner and a valid position value

--- test/unit/page_test.rb
+++ test/unit/page_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class PageTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "a page must be owned by a user" do
+    page = Page.create
+    assert page.invalid?
+    assert page.errors[:user].include?("can't be blank")
+  end
+  
+  test "page position must be set, integral and >=0" do
+    page = Page.create
+    assert page.invalid?
+    assert_not_same 0, page.errors[:position].size
+    page.position = "wibble"
+    
+    assert page.invalid?
+    assert_not_same 0, page.errors[:position].size
+  end
 end
