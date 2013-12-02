2f97734180e244d4c4a2f04f9d3a5e96d93241b6
Stuart Swindells <github@lumo.me.uk>
2013-12-02 01:27:23 +0000

    Update Page unit test with more tests of position validation

--- test/unit/page_test.rb
+++ test/unit/page_test.rb
@@ -11,10 +11,22 @@ class PageTest < ActiveSupport::TestCase
   test "page position must be set, integral and >=0" do
     page = Page.create
     assert page.invalid?
-    assert_not_same 0, page.errors[:position].size
-    page.position = "wibble"
+    assert page.errors[:position].include?("is not a number")
     
+    page.position = "wibble"
     assert page.invalid?
-    assert_not_same 0, page.errors[:position].size
+    assert page.errors[:position].include?("is not a number")
+    
+    page.position = -1
+    assert page.invalid?
+    assert page.errors[:position].include?("must be greater than or equal to 0")
+    
+    page.position = 0
+    page.valid?
+    assert page.errors[:position].empty?
+    
+    page.position = 100
+    page.valid?
+    assert page.errors[:position].empty?
   end
 end
