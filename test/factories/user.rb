d17e4f7a0225b88e0030cfd9f5f3a3101073a3ac
tolien <tolien@users.noreply.github.com>
2013-12-03 23:40:51 +0000

    Add a factory for generating test users

---
+++ test/factories/user.rb
@@ -0,0 +1,10 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :user do
+    username { SecureRandom.urlsafe_base64 12 }
+    email { "#{SecureRandom.urlsafe_base64 12}@#{SecureRandom.urlsafe_base64 12}.#{SecureRandom.urlsafe_base64 3}" } 
+    password { SecureRandom.urlsafe_base64 12 }
+    password_confirmation { "#{password}" }
+  end
+end
