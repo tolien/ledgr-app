05fcd36e7b7bb45438a46729ecc89df4f09d2c8f
tolien <tolien@users.noreply.github.com>
2013-12-04 01:15:31 +0000

    Add association between pages and displays, the other half of 4e1e232

--- app/models/page.rb
+++ app/models/page.rb
@@ -2,9 +2,14 @@ class Page < ActiveRecord::Base
   extend FriendlyId
   friendly_id :title, use: :slugged
   
+  acts_as_list
+  
   attr_accessible :title
   
+  default_scope order ("position ASC")
+  
   belongs_to :user
+  has_many :displays
   
   validates_presence_of :user
   validates_numericality_of :position, only_integer: true, greater_than_or_equal_to: 0
