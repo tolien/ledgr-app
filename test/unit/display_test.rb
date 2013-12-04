854667262275130af8727e9521e6191d9192a399
tolien <tolien@users.noreply.github.com>
2013-12-04 02:31:39 +0000

    Unit tests for displays

--- test/unit/display_test.rb
+++ test/unit/display_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class DisplayTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @display_type = DisplayType.create
+    @page = FactoryGirl.build(:page)
+  end
+  
+  test "a Display must be associated with a display type and page" do
+    display = Display.create
+    assert display.invalid?
+    
+    
+    display.display_type = @display_type
+    display.page = @page
+    
+    assert display.valid?
+  end
 end
