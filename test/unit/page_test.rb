6dd416cc2d862533088ce2f12e01c70c2987dcb4
tolien <tolien@users.noreply.github.com>
2013-12-06 22:39:30 +0000

    Unit test that pages are created at the end of the list

--- test/unit/page_test.rb
+++ test/unit/page_test.rb
@@ -1,6 +1,9 @@
 require 'test_helper'
 
 class PageTest < ActiveSupport::TestCase
+  setup do
+    @user = FactoryGirl.create(:user)
+  end
   
   test "a page must be owned by a user" do
     page = FactoryGirl.build(:page)
@@ -26,5 +29,22 @@ class PageTest < ActiveSupport::TestCase
     page.position = 100
     page.valid?
     assert page.errors[:position].empty?
+  end
+  
+  test "pages are loaded in the correct order" do
+    page_list = []
+    
+    10.times do
+      page = FactoryGirl.build(:page)
+      page.user = @user
+      page.save!
+      page_list.append(page.title)
+    end
+    
+    page_list.reverse
+    @user.pages.each do |page|
+      title = page_list.pop
+      assert_equal title, page.title
+    end
   end
 end
