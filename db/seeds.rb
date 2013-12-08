d8d0a8eedab28c4a5bdd40be66dd2d6ca6031bd1
tolien <tolien@users.noreply.github.com>
2013-12-08 02:03:22 +0000

    Add seed data: user, items, entries, categories, pages

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+user = User.first_or_create password: 'password', password_confirmation: 'password', email: 'blackhole@data-tracker.co.uk', username: 'test_user'
+
+20.times do |index|
+  item = Item.where(name: "Item #{index}", user_id: user.id).first_or_create
+  item.user = user
+  item.save!
+  
+  rand(20).times do
+    entry = Entry.new
+    entry.item = item
+    entry.quantity = rand(5)
+    entry.datetime = DateTime.current
+    entry.save!
+  end
+  
+  category = Category.where(user_id: user.id, name: "Category #{index % 4}").first_or_create
+  item.add_category(category)
+end
+
+5.times do |index|
+  page = Page.where(title: "Page #{index}", user_id: user.id).first_or_create
+  page.save!
+end
+
