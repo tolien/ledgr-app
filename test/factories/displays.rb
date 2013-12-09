937ffe8697e95509e9697f133430868a778a0507
Stuart Swindells <github@lumo.me.uk>
2013-12-09 11:33:46 +0000

    Add factory for displays

---
+++ test/factories/displays.rb
@@ -0,0 +1,9 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :display do
+    title "MyText"
+    start_date 1.month.ago
+    end_date DateTime.current
+  end
+end
