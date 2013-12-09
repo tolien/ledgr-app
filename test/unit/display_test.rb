7196e7c16db24b86d822b974c496cb40fc7c4ab9
tolien <tolien@users.noreply.github.com>
2013-12-09 12:03:17 +0000

    Use FactoryGirl to build display type object

--- test/unit/display_test.rb
+++ test/unit/display_test.rb
@@ -2,14 +2,13 @@ require 'test_helper'
 
 class DisplayTest < ActiveSupport::TestCase
   setup do
-    @display_type = DisplayType.create
+    @display_type = FactoryGirl.build(:display_type)
     @page = FactoryGirl.build(:page)
   end
   
   test "a Display must be associated with a display type and page" do
     display = Display.create
     assert display.invalid?
-    
     
     display.display_type = @display_type
     display.page = @page
