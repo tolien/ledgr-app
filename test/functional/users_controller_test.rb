4246a0c0f5df2ea3d940eca5814a099e8fd6102f
Stuart Swindells <github@lumo.me.uk>
2014-01-02 02:19:14 +0000

    Assert that the /username shows the user's first page, if one is defined

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -10,4 +10,17 @@ class UsersControllerTest < ActionController::TestCase
     assert_response :success
     assert_template :show
   end
+  
+  test "should show first page, if user has one" do
+    5.times do |index|
+      page = FactoryGirl.build(:page)
+      page.title = index
+      page.user = @user
+      page.save!
+    end
+    
+    get :show, id: @user.id
+    assert_equal '0', assigns[:page].title
+    
+  end
 end
