d3991766cdc548aee263c3265d78b817594b065a
tolien <tolien@users.noreply.github.com>
2014-01-20 00:34:17 +0000

    If the user has a page, assert that the first one gets shown

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,9 @@ class UsersController < ApplicationController
   # GET /1.json
   def show
     @user = User.find(params[:id])
+    if (@user.pages.size > 0)
+      @page = @user.pages.first
+    end
 
     respond_to do |format|
       format.html # show.html.erb
