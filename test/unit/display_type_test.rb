a4b10a390651fb064112db43c91e25e50a1dac4b
tolien <tolien@users.noreply.github.com>
2014-02-24 12:35:53 +0000

    Assert that destroying a display type destroys its dependent displays

--- test/unit/display_type_test.rb
+++ test/unit/display_type_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class DisplayTypeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = FactoryGirl.create(:user)
+    @type = FactoryGirl.create(:display_type)
+    @page = FactoryGirl.build(:page)
+    @page.user = @user
+    @page.save!
+    
+    5.times do
+      @display = FactoryGirl.build(:display)
+      @display.page = @page
+      @display.display_type = @type
+      @display.save!
+    end    
+        
+  end
+  
+  test "destroying a display type destroys its dependent displays" do
+    # assert that there are displays beforehand, or this test is pointless
+    assert !Display.where('display_type_id = ?', @type.id).empty?
+    @type.destroy
+    assert Display.where('display_type_id = ?', @type.id).empty?
+  end
 end
