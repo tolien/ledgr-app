31a950473adb2e4c08c3d1764d11093a775b0cbe
Stuart Swindells <github@lumo.me.uk>
2014-04-08 15:29:54 +0100

    Use user ID, add a test importing a single item

--- test/integration/import_test.rb
+++ test/integration/import_test.rb
@@ -16,6 +16,10 @@ class ImportTest < ActionDispatch::IntegrationTest
         categories: ['drinks', 'alcohol']
       }
     ]
+    
+    @single_import = []
+    @single_import << @test_import[0]
+    
     @tricky_import = [
       {
       name: 'orange',
@@ -28,22 +32,30 @@ class ImportTest < ActionDispatch::IntegrationTest
     ]
   end
   
+  test "single item import" do
+    assert_difference('@user.categories.size', 1) do
+      assert_difference('@user.items.size', 1) do
+        import_item_categories(@user.id, @single_import)
+      end
+    end
+  end
+  
   test "items and categories imported properly" do
     assert_difference('@user.categories.size', 2) do
       assert_difference('@user.items.size', 2) do
-        import_item_categories(@user, @test_import)
+        import_item_categories(@user.id, @test_import)
       end
     end    
   end
   
   test "two items with identical names but different categories are created" do
     assert_difference('@user.items.size', 2) do
-      import_item_categories(@user, @tricky_import)    
+      import_item_categories(@user.id, @tricky_import)    
     end    
   end
   
   test "category-item associations set up properly" do
-    import_item_categories(@user, @test_import)
+    import_item_categories(@user.id, @test_import)
     @test_import.each do |item|
       
     end
