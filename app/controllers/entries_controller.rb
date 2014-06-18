fd4295700793a1cfe29be63d804aa4fbbddfbc81
Stuart Swindells <github@lumo.me.uk>
2014-06-18 11:27:12 +0100

    Eagerly load item categories when loading entries in Entry.index controller

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -4,7 +4,7 @@ class EntriesController < ApplicationController
   # GET /entries.json
   def index
     @user = User.find(params[:user_id])
-    @entries = @user.entries.includes(:item).order("datetime desc").paginate(page: params[:page])
+    @entries = @user.entries.includes(item: [:categories]).order("datetime desc").paginate(page: params[:page])
 
     respond_to do |format|
       format.html # index.html.erb
