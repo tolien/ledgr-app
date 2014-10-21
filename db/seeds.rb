b02d91de237051841789924386fdf4d7ecabb6e2
tolien <tolien@users.noreply.github.com>
2014-10-21 11:12:49 +0100

    Update password confirmation

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create(username: 'test_user', password: 'passw0rd', password_confirmation: 'password', email: 'blackhole@ledgr-app.com')
+User.create(username: 'test_user', password: 'passw0rd', password_confirmation: 'passw0rd', email: 'blackhole@ledgr-app.com')
 
