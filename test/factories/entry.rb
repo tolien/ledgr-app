d75e438184a889185ef3569b41908fd020970ae1
tolien <tolien@users.noreply.github.com>
2015-01-02 18:40:45 +0000

    Fix intermittant test failures with MySQL <5.6

--- test/factories/entry.rb
+++ test/factories/entry.rb
@@ -5,5 +5,11 @@ FactoryGirl.define do
     item
     quantity { SecureRandom.random_number(10) + SecureRandom.random_number }
     datetime { DateTime.now.to_s }
+    # MySQL <5.6 doesn't use millisecond precision in time values
+    # (http://dev.mysql.com/doc/refman/5.6/en/fractional-seconds.html)
+    # so wind the created_at and updated_at datetimes back to stop intermittant test fallures
+    # where the 'last created' or 'last modified' entry is accessed
+    created_at { DateTime.now - 2.seconds }
+    updated_at { DateTime.now - 2.seconds }
   end
 end
