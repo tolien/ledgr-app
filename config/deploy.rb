6eea80830314646531cf5baa161f8334ed5eabe1
tolien <tolien@users.noreply.github.com>
2015-01-06 13:38:39 +0000

    Use environment variable to set which branch to deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,6 +6,7 @@ set :repo_url, 'git@github.com:tolien/data-tracker.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+set :branch, ENV['BRANCH'] || :master
 
 # Default deploy_to directory is /var/www/my_app
 set :deploy_to, ->() { "/var/sites/#{fetch(:application)}" }
