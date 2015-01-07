e00334bb6a2f1eb75e46a035a7a1a3d42e15e469
tolien <tolien@users.noreply.github.com>
2015-01-07 01:05:58 +0000

    Remove user_id param nested inside category from tests for items and categories

--- test/functional/items_controller_test.rb
+++ test/functional/items_controller_test.rb
@@ -125,15 +125,19 @@ class ItemsControllerTest < ActionController::TestCase
   
   test "shouldn't be able to create an item for another user" do
     sign_in @user
+    
+    get :new, user_id: @user2.id
+    assert_response(:forbidden)
+        
     assert_no_difference('Item.count') do
-      post :create, item: { name: @item.name + "_new", user_id: @user2.id }, user_id: @user2.id
+      post :create, item: { name: @item.name + "_new" }, user_id: @user2.id
     end
     assert_response(:forbidden)
   end
   
   test "shouldn't be able to update an item belonging to another user" do
     sign_in @user2
-    put :update, id: @item, item: { name: @item.name + "_changed", user_id: @user.id }, user_id: @user.id
+    put :update, id: @item, item: { name: @item.name + "_changed" }, user_id: @user.id
     assert_response(:forbidden)
     assert_equal @item, assigns(:item)
   end
