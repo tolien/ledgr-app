30c048fd19629b6706199986d26f2af57d75e596
Stuart Swindells <github@lumo.me.uk>
2015-02-02 20:16:55 +0000

    Handle the last entry datetime being returned as a Time, as happens with PG/MySQL

--- app/models/display_types/time_since_last_entry.rb
+++ app/models/display_types/time_since_last_entry.rb
@@ -3,8 +3,10 @@ class DisplayTypes::TimeSinceLastEntry < DisplayType
   def get_data_for(display)
     data = super(display)
     max_date = data.maximum("entries.datetime")
-    unless max_date.nil?
-      Time.parse(max_date)
+    unless max_date.nil? or max_date.is_a? Time
+      max_date = Time.parse(max_date)
+    end
+    max_date
     end
   end
 end
