c2ae53554a41c8c0c97b10c71e965afe6ca0633f
tolien <tolien@users.noreply.github.com>
2015-02-02 19:57:32 +0000

    Fix test failure

--- app/models/display_types/time_since_last_entry.rb
+++ app/models/display_types/time_since_last_entry.rb
@@ -3,7 +3,7 @@ class DisplayTypes::TimeSinceLastEntry < DisplayType
   def get_data_for(display)
     data = super(display)
     max_date = data.maximum("entries.datetime")
-    unless max_date.nil? or max_date.empty?
+    unless max_date.nil?
       Time.parse(max_date)
     end
   end
