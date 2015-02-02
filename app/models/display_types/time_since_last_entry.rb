d7b5da0c33d68fc145f8d2ed1ffa9564c78a7f5c
Stuart Swindells <github@lumo.me.uk>
2015-02-02 17:07:06 +0000

    Convert the returned datetime from a string into a Time object

--- app/models/display_types/time_since_last_entry.rb
+++ app/models/display_types/time_since_last_entry.rb
@@ -2,6 +2,9 @@ class DisplayTypes::TimeSinceLastEntry < DisplayType
   
   def get_data_for(display)
     data = super(display)
-    data.maximum("entries.datetime")
+    max_date = data.maximum("entries.datetime")
+    unless max_date.nil? or max_date.empty?
+      Time.parse(max_date)
+    end
   end
 end
