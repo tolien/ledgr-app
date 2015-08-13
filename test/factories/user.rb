c9870e49370a64077ee1de3b6701fda86efda429
tolien <tolien@users.noreply.github.com>
2015-08-13 21:22:38 +0000

    Generate more characters for TLD but limit to 3 chars

--- test/factories/user.rb
+++ test/factories/user.rb
@@ -3,7 +3,7 @@
 FactoryGirl.define do
   factory :user do
     username { SecureRandom.urlsafe_base64 12 }
-    email { "#{SecureRandom.urlsafe_base64 12}@#{SecureRandom.urlsafe_base64 12}.#{SecureRandom.urlsafe_base64(3).gsub(/[^a-zA-Z]/, '')}" } 
+    email { "#{SecureRandom.urlsafe_base64 12}@#{SecureRandom.urlsafe_base64 12}.#{SecureRandom.urlsafe_base64(10).gsub(/[^a-zA-Z]/, '').slice(0,3)}" } 
     password { SecureRandom.urlsafe_base64 12 }
     password_confirmation { "#{password}" }
   end
