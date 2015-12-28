61e4c5cbafc38a28d0839029dd566a2aa36dea2a
Stuart Swindells <github@lumo.me.uk>
2015-12-28 18:20:10 +0000

    Reset column information after adding unconfirmed_email column

--- db/migrate/20140903215931_add_unconfirmed_email_to_users.rb
+++ db/migrate/20140903215931_add_unconfirmed_email_to_users.rb
@@ -1,9 +1,11 @@
 class AddUnconfirmedEmailToUsers < ActiveRecord::Migration
   def up
       add_column :users, :unconfirmed_email, :string
+      User.reset_column_information
   end
   
   def down
       remove_columns :users, :unconfirmed_email
+      User.reset_column_information
   end
 end
