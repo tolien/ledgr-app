62f6716a5701ef86a29f67fad9a413e5e30e8601
tolien <tolien@users.noreply.github.com>
2016-01-14 16:16:15 +0000

    Average time between entries display type and some tests

---
+++ app/models/display_types/average_time_between_entry.rb
@@ -0,0 +1,14 @@
+class DisplayTypes::AverageTimeBetweenEntry < DisplayType
+
+  def get_data_for(display)
+    data = super(display).select("max(entries.datetime) as last_entry, min(entries.datetime) as first_entry, count(*) as entry_count").first
+    count = data.entry_count
+    
+    unless count == 0
+      max_date = data.last_entry.to_datetime
+      min_date = data.first_entry.to_datetime
+      delta = (max_date.to_date - min_date.to_date) / count
+      delta.to_f
+    end
+  end
+end
