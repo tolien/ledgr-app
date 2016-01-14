cb7c75fe22d76dae373fcbeffbf142ae8154e361
Stuart Swindells <github@lumo.me.uk>
2016-01-14 21:54:12 +0000

    Blah

--- app/models/display_types/average_time_between_entry.rb
+++ app/models/display_types/average_time_between_entry.rb
@@ -4,10 +4,13 @@ class DisplayTypes::AverageTimeBetweenEntry < DisplayType
     data = super(display)
     data = data.unscope(:select)
     data = data.unscope(:order)
-    data = data .select("max(entries.datetime) as last_entry, min(entries.datetime) as first_entry, count(*) as entry_count").first
-    count = data.entry_count
+    data = data.select("max(entries.datetime) as last_entry, min(entries.datetime) as first_entry, count(*) as entry_count")
+    unless data.nil? or data.empty?
+      data = data[0]
+      count = data.entry_count
+    end
     
-    unless count == 0
+    unless count.nil? or count == 0
       max_date = data.last_entry.to_datetime
       min_date = data.first_entry.to_datetime
       delta = (max_date.to_date - min_date.to_date) / count
