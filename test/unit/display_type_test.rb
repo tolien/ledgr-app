a6dc62c6600c151c33b87b803f911ebf4cb086c9
tolien <tolien@users.noreply.github.com>
2016-01-15 15:26:30 +0000

    Move test to time since last entry display type and fix error

--- test/unit/display_type_test.rb
+++ test/unit/display_type_test.rb
@@ -23,27 +23,4 @@ class DisplayTypeTest < ActiveSupport::TestCase
     @type.destroy
     assert Display.where('display_type_id = ?', @type.id).empty?
   end
-  
-    test "start date constraints" do
-    category = FactoryGirl.create(:category, user: @user)
-    display = @page.displays.first
-    display.categories << category
-    display.start_date = 1.days.ago
-
-    item = FactoryGirl.create(:item, user: @user)
-    category.items << item
-    
-    entry = FactoryGirl.create(:entry, item: item, datetime: 5.days.ago)
-    
-    result = display.get_data
-    assert_nil result
-
-    display.start_date = 20.days.ago
-    display.save!
-    
-    entry.reload
-    result = display.get_data
-    assert_equal entry.datetime.to_time, result
-    
-  end
 end
