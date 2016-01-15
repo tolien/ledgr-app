596df58921d57ef9fb0621f09cf26b077b6ba742
Stuart Swindells <github@lumo.me.uk>
2016-01-15 14:31:16 +0000

    Tests for start datetime constraints on displays

--- test/unit/display_type_test.rb
+++ test/unit/display_type_test.rb
@@ -9,10 +9,10 @@ class DisplayTypeTest < ActiveSupport::TestCase
     @page.save!
     
     5.times do
-      @display = FactoryGirl.build(:display)
-      @display.page = @page
-      @display.display_type = @type
-      @display.save!
+      display = FactoryGirl.build(:display)
+      display.page = @page
+      display.display_type = @type
+      display.save!
     end    
         
   end
@@ -22,5 +22,28 @@ class DisplayTypeTest < ActiveSupport::TestCase
     assert !Display.where('display_type_id = ?', @type.id).empty?
     @type.destroy
     assert Display.where('display_type_id = ?', @type.id).empty?
+  end
+  
+    test "start date constraints" do
+    category = FactoryGirl.create(:category, user: @user)
+    display = @page.displays.first
+    display.categories << category
+    display.start_date = 1.days.ago
+
+    item = FactoryGirl.create(:item, user: @user)
+    category.items << item
+    
+    entry = FactoryGirl.create(:entry, item: item, datetime: 5.days.ago)
+    
+    result = display.get_data
+    assert_nil result
+
+    display.start_date = 20.days.ago
+    display.save!
+    
+    entry.reload
+    result = display.get_data
+    assert_equal entry.datetime.to_time, result
+    
   end
 end
