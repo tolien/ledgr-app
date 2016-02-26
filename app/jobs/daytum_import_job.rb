0a08620a7c76248f8979ba3c4a3714dd7656e413
tolien <tolien@users.noreply.github.com>
2016-02-26 00:20:56 +0000

    Initial commit of an ActiveJob job to import CSVs

--- app/jobs/daytum_import_job.rb
+++ app/jobs/daytum_import_job.rb
@@ -1,7 +1,11 @@
 class DaytumImportJob < ActiveJob::Base
   queue_as :default
 
-  def perform(*args)
-    # Do something later
+  def perform(user_id, file)
+    importer = Importer.new
+    user = User.find(user_id)
+    unless user.nil?
+      importer.import file, user
+    end
   end
 end
