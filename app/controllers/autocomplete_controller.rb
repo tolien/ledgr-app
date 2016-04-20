0f67191b443a5df9fab130100fb8810d2e735ba5
Stuart Swindells <github@lumo.me.uk>
2016-04-21 00:26:53 +0100

    JQuery-UI autocomplete uses the parameter term, update the controller to match

--- app/controllers/autocomplete_controller.rb
+++ app/controllers/autocomplete_controller.rb
@@ -3,7 +3,8 @@ class AutocompleteController < ApplicationController
   
   def index
     @user = current_user
-    @items = @user.items.where('name like :name', {name: "%#{params[:name]}%"}).reorder(entries_count: :desc).pluck(:name)
+    name = params[:term]
+    @items = @user.items.where('name like :name', {name: "%#{name}%"}).reorder(entries_count: :desc).pluck(:name)
 
     respond_to do |format|
       format.json
