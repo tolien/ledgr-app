478f2254234022fe947c03c66cb0dff9b9fe5ab9
tolien <tolien@users.noreply.github.com>
2016-04-29 15:32:49 +0000

    Set JS compressor to uglifier for minification

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,6 +13,8 @@ LedgrApp::Application.configure do
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
+  config.assets.js_compressor = :uglifier
+  # config.assets.css_compressor = :sass
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
