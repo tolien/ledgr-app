b923b9c143c4ba4c5e605a5fbc45a36776a72918
tolien <tolien@users.noreply.github.com>
2016-05-09 09:19:44 +0000

    Add delayed_jobs table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140928230755) do
+ActiveRecord::Schema.define(version: 20160503111256) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -21,6 +21,22 @@ ActiveRecord::Schema.define(version: 20140928230755) do
   end
 
   add_index "categories", ["name", "user_id"], name: "index_categories_on_name_and_user_id", unique: true
+
+  create_table "delayed_jobs", force: :cascade do |t|
+    t.integer  "priority",   default: 0, null: false
+    t.integer  "attempts",   default: 0, null: false
+    t.text     "handler",                null: false
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.string   "queue"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority"
 
   create_table "display_categories", force: :cascade do |t|
     t.integer  "category_id", null: false
