52e6ce4ef9c8b3adf43b9b5f80c7ff71728e3c44
Stuart Swindells <github@lumo.me.uk>
2016-07-01 07:57:37 +0000

    Enable 2FA for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ActiveRecord::Base
+  devise :two_factor_authenticatable,
+         :otp_secret_encryption_key => ENV['2FA_KEY']
+
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
          
   extend FriendlyId
