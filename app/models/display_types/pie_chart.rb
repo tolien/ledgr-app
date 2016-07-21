1cf9b654777756d73a660bf03958b3402b579e0b
tolien <tolien@users.noreply.github.com>
2016-07-21 11:40:47 +0000

    Need to assign grouped/reordered data

--- app/models/display_types/pie_chart.rb
+++ app/models/display_types/pie_chart.rb
@@ -2,7 +2,9 @@ class DisplayTypes::PieChart < DisplayType
 
   def get_data_for(display)
     data = super(display)
-    data.group(:name)
-    data.reorder('sum DESC')
+    data = data.group(:name)
+    data = data.reorder('sum DESC')
+    
+    data
   end
 end
