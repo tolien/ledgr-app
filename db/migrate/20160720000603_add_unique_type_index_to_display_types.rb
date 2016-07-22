8ca9dfbc5eb780001cba605e95fc6c1b0fab3162
Stuart Swindells <github@lumo.me.uk>
2016-07-22 12:37:59 +0000

    Set a length on the DisplayTypes type index of 255 characters to fix a MySQL error

--- db/migrate/20160720000603_add_unique_type_index_to_display_types.rb
+++ db/migrate/20160720000603_add_unique_type_index_to_display_types.rb
@@ -1,5 +1,5 @@
 class AddUniqueTypeIndexToDisplayTypes < ActiveRecord::Migration[5.0]
   def change
-    add_index :display_types, :type, unique: true
+    add_index :display_types, :type, unique: true, length: { type: 255 }
   end
 end
