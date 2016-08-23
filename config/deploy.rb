32d7eabc0bd8ccf980c2e392b84d5dadb887bc51
Stuart Swindells <github@lumo.me.uk>
2016-08-23 09:13:33 +0000

    Don't share the bin directory, disable generating binstubs

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,7 @@ set :log_level, :info
  set :linked_files, %w{config/database.yml config/initializers/secret_token.rb}
 
 # Default value for linked_dirs is []
- set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/assets}
+ set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/assets}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
@@ -39,6 +39,9 @@ set :bundle_jobs, 4
 
 # set Rails environment to production
 set :rails_env, "production"
+
+# no binstubs
+set :bundle_binstubs, nil
 
 # delayed_job config
 set :delayed_job_workers, 1
