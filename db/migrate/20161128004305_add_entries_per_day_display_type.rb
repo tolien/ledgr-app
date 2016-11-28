d11aadb491c7d9fc90399f590ddcf394cd6cf0af
tolien <tolien@users.noreply.github.com>
2016-11-28 00:46:18 +0000

    Add entries per day display type to DB

---
+++ db/migrate/20161128004305_add_entries_per_day_display_type.rb
@@ -0,0 +1,9 @@
+class AddEntriesPerDayDisplayType < ActiveRecord::Migration[5.0]
+  def change
+    display_type = DisplayTypes::EntriesPerDay.find_or_create_by(name: 'Entries per Day')
+    display_type.save!
+      end
+    end
+
+  end
+end
