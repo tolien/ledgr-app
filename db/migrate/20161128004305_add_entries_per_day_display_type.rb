c21d4bf4417fb0e1c7120557f3fb118dc3a5b6b8
tolien <tolien@users.noreply.github.com>
2016-11-28 16:05:19 +0000

    Also update migration

--- db/migrate/20161128004305_add_entries_per_day_display_type.rb
+++ db/migrate/20161128004305_add_entries_per_day_display_type.rb
@@ -1,6 +1,6 @@
-class AddEntriesPerDayDisplayType < ActiveRecord::Migration[5.0]
+class AddDayAverageDisplayType < ActiveRecord::Migration[5.0]
   def change
-    display_type = DisplayTypes::EntriesPerDay.find_or_create_by(name: 'Entries per Day')
+    display_type = DisplayTypes::DayAverage.find_or_create_by(name: 'Day Average')
     display_type.save!
   end
 end
