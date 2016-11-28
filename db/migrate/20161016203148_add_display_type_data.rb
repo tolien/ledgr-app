fcd77120d84deab3d58a8898d151ba1e443faea7
Stuart Swindells <github@lumo.me.uk>
2016-11-28 00:42:48 +0000

    Use find_or_create_by to create display types

--- db/migrate/20161016203148_add_display_type_data.rb
+++ db/migrate/20161016203148_add_display_type_data.rb
@@ -1,9 +1,9 @@
 class AddDisplayTypeData < ActiveRecord::Migration[5.0]
   def change
-    time_since_last_entry_display_type = DisplayTypes::TimeSinceLastEntry.create(name: 'Time Since Last Entry')
+    time_since_last_entry_display_type = DisplayTypes::TimeSinceLastEntry.find_or_create_by(name: 'Time Since Last Entry')
     time_since_last_entry_display_type.save!
     
-    time_between_entries_display_type = DisplayTypes::AverageTimeBetweenEntry.create(name: 'Time Between Entries')
+    time_between_entries_display_type = DisplayTypes::AverageTimeBetweenEntry.find_or_create_by(name: 'Time Between Entries')
     time_between_entries_display_type.save!
   end  
 end
