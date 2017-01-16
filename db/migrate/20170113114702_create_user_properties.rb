e64939bddc4cb00c4cf91c53e7bd4acd4e8a7fe5
Stuart Swindells <github@lumo.me.uk>
2017-01-16 00:17:25 +0000

    Get rid of the user_properties database table and instead store is_private as a property on User

--- db/migrate/20170113114702_create_user_properties.rb
+++
@@ -1,8 +0,0 @@
-class CreateUserProperties < ActiveRecord::Migration[5.0]
-  def change
-    create_table :user_properties do |t|
-      t.references :user, index: true, foreign_key: true
-      t.boolean :is_private, default: false
-    end
-  end
-end
