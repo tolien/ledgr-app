711051626b9af566b4ebc4aabf0f12770f978112
Stuart Swindells <github@lumo.me.uk>
2017-02-13 12:32:13 +0000

    Make export faster Significant time used in the export was being used by AR turning database rows into objects Load items/categories once per export, then pluck item ID, quantity, datetime from the entries table, removing the join to Categories altogether

--- app/import-export/exporter.rb
+++ app/import-export/exporter.rb
@@ -8,22 +8,46 @@ class Exporter < Object
   end
   
   def export(user_id)
+    Rails.logger.info "Beginning export for user ID #{user_id}"
+    start_time = Time.now
+    
     user = User.find(user_id)
     
     unless user.nil?
-      csv_string = CSV.generate do |csv|
-        row = []
-        Entry.includes(item: [:categories]).where(items: { user_id: user_id }).find_each do |entry|
-          row << entry.item.name
-          row << entry.datetime.utc
-          row << entry.quantity.to_f
-          unless entry.item.categories.empty?
-            row << entry.item.categories.pluck(:name).join(',')
-          end
-        end
-        csv << row
+      csv_string = ""
+      csv_string << "name,date,amount,categories\n"
+      
+      item_map = {}
+      
+      Item.includes(:categories).where(user_id: user_id).each do |item|
+        item_map[item.id] = item  
       end
+      
+      Rails.logger.info "Finished mapping user's items/categories after #{Time.now - start_time} seconds"
+      
+      Entry
+        .includes(:item)
+        .where(items: { user_id: user_id })
+        .pluck(:item_id, :datetime, :quantity)
+        .each do |entry|
+          row = []
+          
+          item_id = entry[0]
+          datetime = entry[1]
+          quantity = entry[2]
+          
+          item = item_map[item_id]
+          row << item.name
+          row << datetime.utc.strftime('%a %b %d %T %Z %Y')
+          row << quantity.to_f
+          unless item.categories.empty?
+            row << item.categories.pluck(:name).join(',')
+          end
+          csv_string << row.to_csv
+        end
+      Rails.logger.info "Finished export after #{Time.now - start_time} seconds"
       csv_string
+      
     end  
   end
 end
