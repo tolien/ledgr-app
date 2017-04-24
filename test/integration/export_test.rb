dd7c41fd4b13ff09f7e575eba9711426363165f3
tolien <tolien@users.noreply.github.com>
2017-04-24 21:07:47 +0100

    Should be checking if testfiles exists before deleting it, not deleting if it doesn't exist...

--- test/integration/export_test.rb
+++ test/integration/export_test.rb
@@ -9,7 +9,7 @@ class ExportTest < ActionDispatch::IntegrationTest
     @entry = FactoryGirl.build(:entry, item: @item)
     @exporter = Exporter.new
     @tempdir = Rails.root.join('tmp').join('testfiles')
-    unless @tempdir.exist?
+    if @tempdir.exist?
       @tempdir.mkdir
     end
   end
