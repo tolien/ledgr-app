eb4393580209f0691dee398c24ca7d8ca0cc5d32
tolien <tolien@users.noreply.github.com>
2017-05-16 11:04:54 +0000

    Fix deprecation warnings in migrations

--- db/migrate/20130407165119_add_user_id_to_entries.rb
+++ db/migrate/20130407165119_add_user_id_to_entries.rb
@@ -1,4 +1,4 @@
-class AddUserIdToEntries < ActiveRecord::Migration
+class AddUserIdToEntries < ActiveRecord::Migration[5.0]
   def change
     add_column :entries, :user_id, :integer
     add_foreign_key "entries", "users", :name => "entries_user_id_fk", on_delete: :cascade
--- db/migrate/20140928230755_add_unique_index_on_categories.rb
+++ db/migrate/20140928230755_add_unique_index_on_categories.rb
@@ -1,4 +1,4 @@
-class AddUniqueIndexOnCategories < ActiveRecord::Migration
+class AddUniqueIndexOnCategories < ActiveRecord::Migration[5.0]
   def change
     add_index :categories, [:name, :user_id], unique: true
   end
