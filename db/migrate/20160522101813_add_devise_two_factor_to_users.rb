f38f36837655ad75543e60c94e3cd222da62b27f
Stuart Swindells <github@lumo.me.uk>
2017-08-02 21:08:25 +0000

    Set version for migration

--- db/migrate/20160522101813_add_devise_two_factor_to_users.rb
+++ db/migrate/20160522101813_add_devise_two_factor_to_users.rb
@@ -1,4 +1,4 @@
-class AddDeviseTwoFactorToUsers < ActiveRecord::Migration
+class AddDeviseTwoFactorToUsers < ActiveRecord::Migration[5.0]
   def change
     add_column :users, :encrypted_otp_secret, :string
     add_column :users, :encrypted_otp_secret_iv, :string
