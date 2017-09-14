ff80b887cfd5bb66edd24ad5540ca00914ffca1e
Stuart Swindells <github@lumo.me.uk>
2017-09-14 11:48:32 +0000

    Apply same requirement (must be logged in and current agent) to categories and items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,6 +4,14 @@ class ItemsController < ApplicationController
   # GET /items.json
   def index
     @user = User.friendly.find(params[:user_id])
+
+    if @user.is_private 
+      if current_user.nil? 
+        redirect_to new_user_session_path
+      elsif  current_user.id != @user.id
+        render status: :forbidden
+      end
+    end
     @items = @user.items.includes(:categories)
 
     respond_to do |format|
@@ -15,8 +23,17 @@ class ItemsController < ApplicationController
   # GET /items/1
   # GET /items/1.json
   def show
-    @item = Item.find(params[:id])
     @user = User.friendly.find(params[:user_id])
+
+    if @user.is_private 
+      if current_user.nil? 
+        redirect_to new_user_session_path
+      elsif  current_user.id != @user.id
+        render status: :forbidden
+      end
+    end
+
+    @item = Item.find(params[:id])
     @item_entries = @item.entries.order("datetime desc").paginate(page: params[:page])
 
     respond_to do |format|
