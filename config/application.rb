8185055385ef1c48659f5920708022ca5007b990
tolien <tolien@users.noreply.github.com>
2018-02-05 00:55:00 +0000

    Run app:update with 5.2 RC1

--- config/application.rb
+++ config/application.rb
@@ -12,15 +12,8 @@ module LedgrApp
     config.load_defaults 5.2
 
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-    
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    Rails.application.config.time_zone = 'London'
-
-    # Default date/time formats
-    Rails.application.config.date_format = '%d/%m/%Y'
-    Rails.application.config.time_format = '%-I:%M %p'
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
   end
 end
