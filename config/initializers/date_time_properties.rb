89a8faa2f95767b384391e056b3ea5846034e465
tolien <tolien@users.noreply.github.com>
2018-03-25 16:12:10 +0000

    Move timezone back into the application config

--- config/initializers/date_time_properties.rb
+++ config/initializers/date_time_properties.rb
@@ -1,7 +1,5 @@
-# Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-# Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-Rails.application.config.time_zone = 'London'
-
-# Default date/time formats
-Rails.application.config.date_format = '%d/%m/%Y'
-Rails.application.config.time_format = '%-I:%M %p'
+class Application < Rails::Application
+    # Default date/time formats
+    config.date_format = '%d/%m/%Y'
+    config.time_format = '%-I:%M %p'
+end
