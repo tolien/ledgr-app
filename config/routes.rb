f9167f2822d5f72225581eb5a6c58d9573e4dab9
tolien <tolien@users.noreply.github.com>
2018-06-26 23:03:46 +0000

    First run at making doorkeeper work for OAuth, including some tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  use_doorkeeper
+
+  namespace :api do
+    resources :users, path: '', only: [:show] do
+      resources :displays
+    end
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'static#index'
