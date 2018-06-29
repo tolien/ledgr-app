69bf77d84ffeb194380a82f061e23c4154b4a2a8
Stuart Swindells <github@lumo.me.uk>
2018-06-29 07:25:43 +0000

    Show a list of unrevoked access tokens with a button to revoke them, and add a controller method to do that revocation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,4 +34,22 @@ class UsersController < ApplicationController
     
     send_data csv, {type: :csv, filename: "#{user.username}.csv"}
   end
+
+  def revoke_oauth_token
+    user = User.friendly.find(params[:id])
+    token_string = params[:token]
+    refresh_token_string = params[:refresh_token]
+    
+    unless not current_user.nil? and current_user.id == user.id
+
+    end
+    unless token_string.nil? and refresh_token_string.nil?
+      token = Doorkeeper::AccessToken.find_by("resource_owner_id = ? AND
+        (token = ? OR refresh_token = ?)", user.id, token_string, refresh_token_string)
+
+      unless token.nil?
+        token.revoke
+      end
+    end
+  end
 end
