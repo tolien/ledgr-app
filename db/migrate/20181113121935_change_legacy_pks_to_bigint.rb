791ab540f8dac85ddde21edc045bd3e2c826f719
Stuart Swindells <github@lumo.me.uk>
2018-11-15 00:44:47 +0000

    Remove and re-add foreign keys before converting PKs to bigints

--- db/migrate/20181113121935_change_legacy_pks_to_bigint.rb
+++ db/migrate/20181113121935_change_legacy_pks_to_bigint.rb
@@ -1,6 +1,17 @@
 class ChangeLegacyPksToBigint < ActiveRecord::Migration[5.2]
   def up
     unless ActiveRecord::Base.connection.adapter_name == 'SQLite'
+      remove_foreign_key :categories, :users
+      remove_foreign_key :display_categories, :categories
+      remove_foreign_key :display_categories, :displays
+      remove_foreign_key :displays, :display_types
+      remove_foreign_key :displays, :pages
+      remove_foreign_key :entries, :items
+      remove_foreign_key :item_categories, :items
+      remove_foreign_key :item_categories, :categories
+      remove_foreign_key :items, :users
+      remove_foreign_key :pages, :users
+
       change_column :items, :id, :bigint
       change_column :users, :id, :bigint
       change_column :entries, :id, :bigint
@@ -8,17 +19,28 @@ class ChangeLegacyPksToBigint < ActiveRecord::Migration[5.2]
       change_column :pages, :id, :bigint
       change_column :displays, :id, :bigint
       change_column :display_types, :id, :bigint
+      change_column :item_categories, :id, :bigint
       change_column :categories, :user_id, :bigint
       change_column :display_categories, :category_id, :bigint
       change_column :display_categories, :display_id, :bigint
       change_column :displays, :display_type_id, :bigint
       change_column :displays, :page_id, :bigint
       change_column :entries, :item_id, :bigint
-      change_column :entries, :item_id, :bigint
       change_column :item_categories, :item_id, :bigint
       change_column :item_categories, :category_id, :bigint
       change_column :items, :user_id, :bigint
       change_column :pages, :user_id, :bigint
+
+      add_foreign_key :categories, :users
+      add_foreign_key :display_categories, :categories
+      add_foreign_key :display_categories, :displays
+      add_foreign_key :displays, :display_types
+      add_foreign_key :displays, :pages
+      add_foreign_key :entries, :items
+      add_foreign_key :item_categories, :items
+      add_foreign_key :item_categories, :categories
+      add_foreign_key :items, :users
+      add_foreign_key :pages, :users
 
     end
   end
