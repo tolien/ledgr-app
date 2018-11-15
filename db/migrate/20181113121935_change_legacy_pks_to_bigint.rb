7cbdc96d91154be47dbcd8fce3ac561fdc987ca3
Stuart Swindells <github@lumo.me.uk>
2018-11-15 09:38:09 +0000

    Remove FK to a Doorkeeper table

--- db/migrate/20181113121935_change_legacy_pks_to_bigint.rb
+++ db/migrate/20181113121935_change_legacy_pks_to_bigint.rb
@@ -11,6 +11,7 @@ class ChangeLegacyPksToBigint < ActiveRecord::Migration[5.2]
       remove_foreign_key :item_categories, :categories
       remove_foreign_key :items, :users
      # remove_foreign_key :pages, :users
+      remove_foreign_key :oauth_access_grants, :users
 
       change_column :items, :id, :bigint
       change_column :users, :id, :bigint
