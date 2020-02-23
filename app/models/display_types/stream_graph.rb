3ec0fb52527911a9fcb97a948dd6a96296544cb6
Stuart Swindells <tolien@users.noreply.github.com>
2020-02-23 16:10:58 +0000

    Changes to how dates are truncated for stream graphs

--- app/models/display_types/stream_graph.rb
+++ app/models/display_types/stream_graph.rb
@@ -3,29 +3,31 @@ class DisplayTypes::StreamGraph < DisplayType
 
   def get_data_for(display)
     data = orig_data = super(display)
-    
+
     unless data.empty?
         data = data.reorder('entries.datetime ASC')
         data = data.pluck(Arel.sql('items.id, items.name, entries.quantity AS sum, entries.datetime'))
-        
+
         if display.start_date.nil?
           min_date = data.first[3].to_time
         else
           min_date = display.start_date
         end
-  
-        max_date = DateTime.now
-  
-#        Rails.logger.debug("min date: #{min_date} max date: #{max_date}")
-        
-        days = ((max_date.to_datetime.at_beginning_of_day - min_date.to_datetime.at_beginning_of_day) / 10)
+
+        if display.end_date.nil?
+          max_date = DateTime.now.at_end_of_day
+        else
+          max_date = display.end_date
+        end
+
+        interval_count = 10
+        days = ((max_date.to_datetime.at_end_of_day - min_date.to_datetime.at_beginning_of_day) / interval_count)
         unless days > 0
           days = 1
         end
-        
-#        Rails.logger.debug("Days per interval: #{days}")
+
         data = data.map do |item|
-          { 
+          {
             item_id: item[0],
             name: item[1],
             value: item[2],
@@ -35,7 +37,7 @@ class DisplayTypes::StreamGraph < DisplayType
 
         result = []
         data = data.group_by {|i| i[:date]}
-        
+
         data.each do |date, date_row|
           date_row = date_row.group_by { |i| i[:item_id] }
           date_row.each do |item_id, items|
@@ -50,13 +52,13 @@ class DisplayTypes::StreamGraph < DisplayType
         end
         {data: result, top_items: get_top_items(orig_data)}
     end
-    
+
   end
 
   # this started off doing weird things with rounding
   # and was reimplemented as a while loop
   # but for a sufficiently small value of hours, this loop could take *millions* of iterations
-  
+
   def date_trunc(start_date, hours, date)
     if date > DateTime.now
         date = DateTime.now
@@ -66,11 +68,9 @@ class DisplayTypes::StreamGraph < DisplayType
     intervals = intervals.floor - 1
     start_date = start_date + (intervals * hours).hours
     next_bin = start_date + hours.hours
-
     if next_bin < DateTime.now and ((DateTime.now - next_bin.to_datetime) / (hours.hours / 1.0.days)) >= 0.1
         start_date = next_bin
     end
-    
     start_date
   end
 
@@ -87,7 +87,7 @@ class DisplayTypes::StreamGraph < DisplayType
     unless data.empty?
        data = data.reorder('sum DESC')
     end
-    data    
+    data
   end
-  
+
 end
