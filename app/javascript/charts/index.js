12adcf13e69826849c0756c1f88b0ab423cda38e
tolien <tolien@users.noreply.github.com>
2022-12-27 21:18:10 +0000

    Use importmap references to import the JS that's exported as part of the charts module

--- app/javascript/charts/index.js
+++ app/javascript/charts/index.js
@@ -3,5 +3,5 @@ import d3 from 'd3';
 console.log("charts/index.js loaded")
 window.d3 = d3;
 
-export {default as pie} from './pie'
-export {default as streamgraph} from './streamgraph'
+export {default as pie} from 'charts/pie'
+export {default as streamgraph} from 'charts/streamgraph'
