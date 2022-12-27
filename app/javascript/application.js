cb12b264d566d03c58566cdb3e8edd77f7fb4e14
tolien <tolien@users.noreply.github.com>
2022-12-27 20:14:09 +0000

    Turn the display type JS blocks into modules which allows removing all the import gymnastics from application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,13 +1,3 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 
-import d3 from "d3"
-import { Turbo } from "@hotwired/turbo-rails"
-
-import { pie, streamgraph } from "charts"
-window.charts = {}
-window.charts.pie = pie
-window.charts.streamgraph = streamgraph
-window.d3 = d3
-
-console.log("application.js loaded")
-console.log(d3.version)
+// import { Turbo } from "@hotwired/turbo-rails"
